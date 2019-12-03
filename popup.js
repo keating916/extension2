9ac75b8b4c4999128ae4c712aca8dc7a041ff993
Patrick Keating <keating916@gmail.com>
2019-12-03 14:39:22 -0800

    Change from local storage to cookies

--- popup.js
+++ popup.js
@@ -3,14 +3,15 @@ let section = document.getElementById("stringSection");
 let p = document.createElement("h1");
 section.appendChild(p);
 let ret = () => {
-    chrome.storage.sync.get(["prifinaKey"], function(result) {
-        let s = `requesting: ${result.prifinaKey}`;
+    chrome.cookies.get({"name": "Prifina"}, function(result) {
+        let s = `requesting: ${result.value}`;
         p.textContent = s;
     })
 }; //returns string set by set() in scripts.js and displays it in the popup
 ret();
 
-
+/*
 setInterval(function() {
     ret()
 }, 1000) // looks for updated String 1/second
+*/
