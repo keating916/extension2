a5efd17e65afb1bbbd146bcbe8b4c98034d5f766
Patrick Keating <keating916@gmail.com>
2019-12-04 14:54:09 -0800

    Include Github

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: ['www.prifina.com', 'www.github.com'], pathContains: 'security'},
+          pageUrl: {hostEquals: 'www.prifina.com', pathContains: 'security'},
+        }),
+		new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'github.com'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
   }
-);  //Only show popup on "Prifina.com/security.html"
+);  //Only show popup on "Prifina.com/security.html and Github.com"
+
+
